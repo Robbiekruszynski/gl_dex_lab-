80aa4570a4bba9fed2351814a8b2a1dd37921ecb
robbie <robbiekruszynski@gmail.com>
2020-12-13 16:22:05 -0800

    4 basic passing tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string public name = "gl";
+    string public name = "Yog";
+    string public symbol = "YOG";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,3 +1,5 @@
+const { result } = require('lodash')
+
 const Token = artifacts.require("./Token")
 
 require('chai')
@@ -5,15 +7,38 @@ require('chai')
 .should()
 
 contract('Token', (accounts) => {
+    const name ='Yog'
+    const symbol = 'YOG'
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+    let token
+
+    //fetch token this allows us to call and chec on token whenever we want to without having to run it each time we run truffle test 
+    beforeEach (async () => {
+        token = await Token.new()
+    })
 
     describe('deployment', () => {
         it('tracks the name', async () => {
-            //fetch token
-           const token =  await Token.new()
             //read token name 
-            const result =await token.name()
+            const result = await token.name()
             //token name is "gl"
-            result.should.equal("gl")
+            result.should.equal(name)
+        })
+
+        it('tracks the symbol', async() => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('tracks the decimals', async() => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it ('tracks the total supply', async() => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
         })
     })
 })
